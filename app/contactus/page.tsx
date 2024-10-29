9b5df3779b4c23ce9cae0aafee289cda544b591c
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-10-29 11:42:55 +0500

    Update page.tsx

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -4,7 +4,7 @@ export default function contactus() {
     <div><h1>Address: House No. L-318, Scheme 33, Diamond City, Karachi, Pakistan</h1>
     <h2>Tell #: (mobile) +92317-2029652,</h2>
     <h3>Email : shaheryarshah2022@gmail.com</h3>
-    <h4>NIC #: 42101-4994249-7</h4>
+   
     <h5>Date of Birth : 22, Jan-1985</h5></div>
     );
    };
